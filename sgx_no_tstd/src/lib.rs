8d3a6da758bc3a74f460abe8aa72db1105e25ecf
Hiroki Chen <etyyuiope@qq.com>
2023-05-12 22:34:55 -0400

    Disable alloc by feature

--- sgx_no_tstd/src/lib.rs
+++ sgx_no_tstd/src/lib.rs
@@ -34,6 +34,7 @@ use sgx_trts::error;
 pub use alloc_crate::alloc::*;
 pub use sgx_alloc::System;
 
+#[cfg(feature = "use_sgx_alloc")]
 #[global_allocator]
 static ALLOC: sgx_alloc::System = sgx_alloc::System;
 
